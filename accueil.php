39fedbe8ae7819719b788782d3b77d529562dfbf
Pan303 <darkamazone303@gmail.com>
2016-04-22 10:23:57 +0200

    sylvie

---
+++ accueil.php
@@ -0,0 +1,25 @@
+
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Accueil</title>
+</head>
+
+<body>
+
+<form action="" method="post" name="pizza">
+
+
+  <p>Que désirez vous sur votre pizza</p>
+  <input name="choix 1" type="checkbox" value="1">fromage<br/>
+  <input name="choix 2" type="checkbox" value="2">jambon<br/>
+  <input name="choix 3" type="checkbox" value="3">champignon<br/>
+  <input name="choix 4" type="checkbox" value="4">salami
+  <br/>
+  <input type="submit" value="Envoyer"/>
+
+</form>
+
+</body>
+</html>
