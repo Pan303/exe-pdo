1b6b174f6b1e62ff9e7cf1e9b2ee3faeb462da3a
vincentdenaes <vi.denaes@laposte.net>
2016-04-22 10:50:29 +0200

    update3

---
+++ View/accueil.inc.php
@@ -0,0 +1,14 @@
+<body>
+
+<form action="" method="post" name="pizza">
+
+
+  <p>Que désirez vous sur votre pizza :</p>
+  <input name="choix 1" type="checkbox" value="1">fromage<br/>
+  <input name="choix 2" type="checkbox" value="2">jambon<br/>
+  <input name="choix 3" type="checkbox" value="3">champignon<br/>
+  <input name="choix 4" type="checkbox" value="4">salami
+  <br/>
+  <input type="submit" value="Envoyer"/>
+
+</form>
