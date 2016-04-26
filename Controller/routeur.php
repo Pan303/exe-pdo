36bbb9d4c137b16c7749a6921603c72c3985eb2d
Pan303 <darkamazone303@gmail.com>
2016-04-26 14:22:20 +0200

    css

--- Controller/routeur.php
+++ Controller/routeur.php
@@ -1,15 +1,11 @@
 <?php
-
 if (empty($_GET)) {
-     
+
      require_once 'View/header.inc.php';
-
-    
-
+     require_once 'View/accueil.inc.php';
      require_once 'View/footer.inc.php';
-     
+
 } elseif (isset($_GET['connect'])) {//ajouter des filtres? autre solution?
      require_once "Model/connect.php";
-     require_once "View/AccueilConnect.inc.php";
-     }
-
+     require_once "View/connect.inc.php";
+}
---
+++ View/accueil.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link type="text/css" rel="stylesheet" href="../css/pizza.css"/>
+  <link href='https://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+  <title>Accueil</title>
+</head>
+
+<body >
+
+<form action="" method="post" name="pizza">
+
+
+  <p>Que désirez-vous sur votre pizza ?</p>
+
+  <input type="checkbox" value="1" name="checkboxG4" id="checkboxG4" class="css-checkbox" />
+  <label for="checkboxG4" class="css-label">jambon</label></br>
+
+  <input type="checkbox" value="2" name="checkboxG5" id="checkboxG5" class="css-checkbox" />
+  <label for="checkboxG5" class="css-label">fromage</label></br>
+
+  <input type="checkbox" value="3" name="checkboxG6" id="checkboxG6" class="css-checkbox" /><label for="checkboxG6" class="css-label">champignon</label></br>
+
+  <input type="checkbox" value="4" name="checkboxG7" id="checkboxG7" class="css-checkbox" /><label for="checkboxG7" class="css-label">salami</label></br>
+
+  <input type="submit" value="Envoyer"/>
+
+</form>
+
+
+
+</body>
+</html>
